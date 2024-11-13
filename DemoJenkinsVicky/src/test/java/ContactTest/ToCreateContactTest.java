ac7447d7de7e44f124b2574f16757afe9e5b1ae1
Neha Singh <Neha Singh@DESKTOP-UQAESM6.mshome.net>
2024-11-13 08:31:58 +0530

    pushed

---
+++ DemoJenkinsVicky/src/test/java/ContactTest/ToCreateContactTest.java
@@ -0,0 +1,10 @@
+package ContactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("Contact created successfully");
+
+	}
+
+}
---
+++ DemoJenkinsVicky/src/test/java/OrganizationTest/ToCreateOrgTest.java
@@ -0,0 +1,10 @@
+package OrganizationTest;
+
+public class ToCreateOrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organization Created Successfully");
+
+	}
+
+}
